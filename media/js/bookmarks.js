e6c429fc454ac933f9dbe4bdea85fef092252202
rhodges <ryan.d.hodges@gmail.com>
2015-07-29 10:19:33 -0700

    updating bookmark url

--- media/js/bookmarks.js
+++ media/js/bookmarks.js
@@ -52,7 +52,7 @@ function bookmarkModel(options) {
     // get the url from a bookmark
     self.getBookmarkUrl = function() {
         var host = window.location.href.split('#')[0];
-        host = 'http://ofr.marineplanner.io/planner/';
+        host = 'http://52.26.64.192/visualize/';
         return host + "#" + self.getBookmarkHash();
         //return host + "#" + self.state;
     };
