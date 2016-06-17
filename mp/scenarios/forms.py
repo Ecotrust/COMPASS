954a9427858c5dadef5e56c05d9bb12de4ac304d
Ryan Hodges <ryan.d.hodges@gmail.com>
2016-06-17 15:15:44 -0700

    style and code updates for drawing and filtering

--- mp/scenarios/forms.py
+++ mp/scenarios/forms.py
@@ -334,7 +334,8 @@ class ScenarioForm(FeatureForm):
     #     return self._get_fields(names)
 
     def get_steps(self):
-        return self.get_step_1_fields()
+
+        return (self.get_step_1_fields(),)
         # return self.get_step_1_fields(), \
         #        self.get_step_2_fields(), \
         #        self.get_step_3_fields(), \
@@ -342,6 +343,7 @@ class ScenarioForm(FeatureForm):
         #        self.get_step_5_fields()
 
     def _get_fields(self, names):
+
         fields = []
         for name_list in names:
             group = []
