62f72680b712f51c03be4be8d735170599c16852
Ryan Hodges <ryan.d.hodges@gmail.com>
2016-06-21 17:21:02 -0700

    adding lookup and improved display of list data in aggregate reports

--- mp/drawing/reports.py
+++ mp/drawing/reports.py
@@ -45,6 +45,41 @@ def get_unique_values(grid_cells, field):
             values.append(value)
     return values
 
+def get_unique_list_values(grid_cells, field):
+    import ast
+    values = []
+    for gc in grid_cells:
+        value = getattr(gc, field)
+        input_list = ast.literal_eval(value)
+        for item in input_list:
+            str_item = str(item)
+            if str_item not in values:
+                values.append(str_item)
+    return values
+
+def apply_lookup(id_list, lookup):
+    values = []
+    for id in id_list:
+        values.append(lookup[id])
+    return values
+
+def unordered_list(list):
+    return "<ul><li>%s</li></ul>" % ("</li><li>".join(list))
+
+#TODO: Populate species lookup when you get it from Mike
+
+species_lookup = {
+    "1": "one",
+    "2": "two",
+    "3": "three",
+    "4": "four",
+    "5": "five",
+    "6": "six",
+    "7": "seven",
+    "8": "eight",
+    "9": "nine",
+    "0": "zero"
+}
 
 def get_summary_reports(grid_cells):
     """
@@ -65,19 +100,23 @@ def get_summary_reports(grid_cells):
 
     # ------- attributes -------
 
-    habitats = get_unique_values(grid_cells, 'habitat')
+    habitats = apply_lookup(get_unique_list_values(grid_cells, 'habitat'), species_lookup)
+    habitats.sort()
     if len(habitats) == 1:
         attributes.append({'title': 'Habitat', 'data': habitats[0]})
     elif len(habitats) > 1:
-        attributes.append({'title': 'Habitats', 'data': ", ".join(habitats)})
+        attributes.append({'title': 'Habitats', 'data': unordered_list(habitats)})
 
-    fish = get_unique_values(grid_cells, 'fish')
-    attributes.append({'title': 'Fish', 'data': ", ".join(fish)})
+    fish = apply_lookup(get_unique_list_values(grid_cells, 'fish'), species_lookup)
+    fish.sort()
+    attributes.append({'title': 'Fish', 'data': unordered_list(fish)})
 
-    obs_spec = get_unique_values(grid_cells, 'obs_spec')
-    attributes.append({'title': 'Observed Species', 'data': ", ".join(obs_spec)})
+    obs_spec = apply_lookup(get_unique_list_values(grid_cells, 'obs_spec'), species_lookup)
+    obs_spec.sort()
+    attributes.append({'title': 'Observed Species', 'data': unordered_list(obs_spec)})
 
-    mod_spec = get_unique_values(grid_cells, 'mod_spec')
-    attributes.append({'title': 'Modeled Species', 'data': ", ".join(mod_spec)})
+    mod_spec = apply_lookup(get_unique_list_values(grid_cells, 'mod_spec'), species_lookup)
+    mod_spec.sort()
+    attributes.append({'title': 'Modeled Species', 'data': unordered_list(mod_spec)})
 
     return attributes
