1e0bfb13721ca700d60f7f39fb4573b2afc31aa6
Ryan Hodges <ryan.d.hodges@gmail.com>
2016-11-14 16:24:21 -0800

    fixing new drawing instersection rules

--- mp/drawing/ofr_manipulators.py
+++ mp/drawing/ofr_manipulators.py
@@ -2,12 +2,17 @@ from scenarios.models import GridCell
 from django.contrib.gis.db.models.aggregates import Union
 
 def clip_to_grid(geom):
+
     intersection = intersecting_cells(geom)
 
     new_shape = intersection.aggregate(Union('geometry'))
-    clipped_shape = new_shape['geometry__union'];
+    if len(intersection) == 1:
+        #single cells come back as a multipolygon
+        clipped_shape = new_shape['geometry__union'].simplify();
+    else:
+        clipped_shape = new_shape['geometry__union'];
 
     return clipped_shape
 
 def intersecting_cells(geom):
-	return GridCell.objects.filter(intersects=geom)
+	return GridCell.objects.filter(geometry__intersects=geom)
